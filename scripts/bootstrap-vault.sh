efdb15e9efa4391c91dda32bb1ae9aa007fa979e
Yevhen Kyriukha <kirgene@gmail.com>
2025-04-23 06:37:58 +0300

    wip

--- scripts/bootstrap-vault.sh
+++ scripts/bootstrap-vault.sh
@@ -1,7 +1,6 @@
 set -euxo pipefail
 # Init vault and save keys
-kubectl get pods
-kubectl get pod vault-0 -o yaml
+kubectl wait --for=condition=Ready pod/vault-0
 kubectl exec vault-0 -- vault operator init -key-shares=1 -key-threshold=1 -format=json > cluster-keys.json
 
 # Store key and unseal vault
