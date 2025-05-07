c740e01705638832969c6e71d72d87ddd602c597
Nguni Phakela <nguni52@gmail.com>
2025-05-07 13:41:56 +0200

    Update hubmanager tests

--- integration_test/tests/src/tests/HubManager.test.ts
+++ integration_test/tests/src/tests/HubManager.test.ts
@@ -4,7 +4,6 @@ import { LandingMenu } from '../page-objects/components/LandingMenu';
 import { HubManagerPage, DFSPRow } from '../page-objects/pages/HubManager';
 import chance from 'chance';
 import { Selector } from 'testcafe';
-
 // NOTE: There are added `t.wait` functions on `t.clicks` to give the UI time to
 //       update. This is to account for observed instances of testcafe
 //       running an assertion or trying to fetch an element before
@@ -17,7 +16,6 @@ fixture `Hub Management Feature`
     await t
       .click(LandingMenu.hubButton).wait(1000);
   });
-
 test.meta({
   ID: '',
   STORY: '',
@@ -26,7 +24,6 @@ test.meta({
   // Navigate to Add DFSP feature
   await t.click(HubManagerPage.hubPageAdministrationDfspSubMenuButton).wait(1000);
   await t.click(HubManagerPage.hubPageAdministrationDfspSubMenuAddDfspButton).wait(1000);
-
   const randomDfspName = chance().company();
   await t.typeText(HubManagerPage.getAddDfspModalNameField, randomDfspName)
   // The UI automatically types out the ID when entering the name
@@ -35,7 +32,6 @@ test.meta({
         .click(Selector('.input-select__options-item__label').withText('Euro'))
   // Submit DFSP
   await t.click(HubManagerPage.getAddDfspModalSubmit).wait(1000);
-
   // Check DFSP exists in data list
   const rows = await HubManagerPage.getDFSPRows();
   const dfspExists = await Promise.all(rows.map((r: DFSPRow) => r.id.innerText));
