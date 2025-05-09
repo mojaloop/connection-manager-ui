f779e62ad43dd2f135ea4f625883d3fcfa98660a
Nguni Phakela <nguni52@gmail.com>
2025-05-09 07:16:39 +0200

    Remove cleanup changes.

--- test/scripts/test-integration.sh
+++ test/scripts/test-integration.sh
@@ -90,12 +90,12 @@ npx testcafe --version
 
 
 # # cleanup 
-cd ${CIRCLE_WORKING_DIRECTORY}/tmp/connection-manager-api/docker
-docker compose down
+# cd ${CIRCLE_WORKING_DIRECTORY}/tmp/connection-manager-api/docker
+# docker compose down
 # #  kill the npm process
 # pkill -f "npm run migrate-and-seed"
 
-rm -rf connection-manager-api
+# rm -rf connection-manager-api
 
 # # restore auth
-sed -i 's/AUTH_ENABLED=false/AUTH_ENABLED=true/' ${PROJECT_ROOT}/connection-manager-api/.env
+# sed -i 's/AUTH_ENABLED=false/AUTH_ENABLED=true/' ${PROJECT_ROOT}/connection-manager-api/.env
