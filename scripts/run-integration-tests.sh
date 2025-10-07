2d61b5c348a7329f86cd17342690f01692da1535
Yevhen Kyriukha <kirgene@gmail.com>
2025-10-07 17:19:34 +0300

    add docker-compose, use mojaloop/build orb for testing

---
+++ scripts/run-integration-tests.sh
@@ -0,0 +1,16 @@
+#!/usr/bin/env bash
+set -e
+
+# Start services and wait for them to be healthy
+docker-compose up -d --wait
+
+# Run the integration tests
+cd integration_test/tests
+CONNECTION_MANAGER_ENDPOINT="http://localhost:3000" npm run test:headless
+TEST_STATUS=$?
+
+# Cleanup
+cd ../..
+docker-compose down -v
+
+exit $TEST_STATUS
