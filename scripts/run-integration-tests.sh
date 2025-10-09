d99c6bf69feda0ec7578b24c74304fd5c2fbe5bb
Yevhen Kyriukha <kirgene@gmail.com>
2025-10-09 21:44:58 +0300

    wip

--- scripts/run-integration-tests.sh
+++ scripts/run-integration-tests.sh
@@ -4,8 +4,9 @@ set -e
 # Start services and wait for them to be healthy
 docker compose up -d --wait
 
-# Run the integration tests
+# Install dependencies and run the integration tests
 cd integration_test/tests
+npm install
 CONNECTION_MANAGER_ENDPOINT="http://localhost:3000" npm run test:headless
 TEST_STATUS=$?
 
