8d0345b10cbb76e31a9452da8feb012ab51182af
Yevhen Kyriukha <kirgene@gmail.com>
2025-10-09 23:46:09 +0300

    wip

--- scripts/run-integration-tests.sh
+++ scripts/run-integration-tests.sh
@@ -7,6 +7,7 @@ docker compose up -d --wait
 # Install dependencies and run the integration tests
 cd integration_test/tests
 npm install
+npx browserslist@latest --update-db
 CONNECTION_MANAGER_ENDPOINT="http://localhost:3000" npm run test:headless
 TEST_STATUS=$?
 
